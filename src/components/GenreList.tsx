14870712e3c01a5a79f132f399de105d5d7d9b81
Rinidh <rinidhdhokia@gmail.com>
2023-08-14 11:37:58 -0700

    Clean up and beautifying of genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,34 +23,42 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List /* this is a list in chakra */>
-      {data.map((genre) => (
-        <ListItem
-          key={genre.id}
-          padding="10px" /* use ListItem from chakra with List of chakra */
-        >
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize={"32px"}
-              borderRadius={8}
-            />
-            <Button
-              fontSize={20}
-              variant={
-                "ghost"
-              } /* adds style to the button, more: solid, link etc */
-              onClick={() => onSelectGenre(genre)} //once clicked, the func set to onSelectGenre at the consumer of this comp will run ie in our case setSelectedGenre in App.tsx will run, with arg 'genre' from here
-              fontWeight={
-                genre.id === selectedGenre?.id ? "extrabold" : "normal"
-              }
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={0}>
+        Genres
+      </Heading>
+      <List /* this is a list in chakra */>
+        {data.map((genre) => (
+          <ListItem
+            key={genre.id}
+            padding="10px" /* use ListItem from chakra with List of chakra */
+          >
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize={"43px"}
+                borderRadius={8}
+                objectFit={"cover"} //"contain" makes the whole img to appear in the 32px by 32px boxSize, "fill" makes the img to occupy all the space (even though cutting off the extremes) , "cover" makes the image filled in the container, but while preserving its ASPECT RATIO
+              />
+              <Button
+                fontSize={20}
+                variant={
+                  "ghost"
+                } /* adds style to the button, more: solid, link etc */
+                onClick={() => onSelectGenre(genre)} //once clicked, the func set to onSelectGenre at the consumer of this comp will run ie in our case setSelectedGenre in App.tsx will run, with arg 'genre' from here
+                fontWeight={
+                  genre.id === selectedGenre?.id ? "extrabold" : "normal"
+                }
+                whiteSpace={"normal"}
+                textAlign={"left"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
