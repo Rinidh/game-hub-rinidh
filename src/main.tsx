cdccabd8d76f4cb7af1c9583e4fc30642aabe811
Rinidh <rinidhdhokia@gmail.com>
2023-08-15 07:04:10 -0700

    Customize themes to get darker grays

--- src/main.tsx
+++ src/main.tsx
@@ -14,9 +14,8 @@ import theme from "./theme";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      {" "}
       {/* theme provided by chakra provider */}
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />{" "}
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       {/* set the ColorModeScript to the configured color mode in the theme.ts file */}
       <App />
     </ChakraProvider>
